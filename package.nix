7899d7d8b92761947a0830dc1dee90a8c148d0b3
Léo Gaspard <leo@gaspard.io>
2021-03-30 00:12:18 +0200

    Use nix from wrapper to make sure the version is compatible

--- package.nix
+++ package.nix
@@ -1,4 +1,4 @@
-{ lib, rustPlatform, openssl, pkg-config }:
+{ lib, rustPlatform, openssl, pkg-config, makeWrapper, nix }:
 
 rustPlatform.buildRustPackage {
   name = "nixpkgs-check";
@@ -6,6 +6,7 @@ rustPlatform.buildRustPackage {
   src = lib.sourceFilesBySuffices ./. [".rs" ".toml" ".lock"];
 
   nativeBuildInputs = [
+    makeWrapper
     pkg-config
   ];
 
@@ -14,4 +15,9 @@ rustPlatform.buildRustPackage {
   ];
 
   cargoSha256 = "0k7m0i7lxzl5hzjlc8jm9r4yh8z6lypynz9xhdjpcyyk8911jmlw";
+
+  postInstall = ''
+    wrapProgram "$out/bin/nixpkgs-check" \
+      --prefix PATH : "${lib.makeBinPath [ nix ]}"
+  '';
 }
